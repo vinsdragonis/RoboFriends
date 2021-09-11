81b786a3650d3abb15baea90ceb5f1f87b13a000
Vineeth B V <37930821+vinsdragonis@users.noreply.github.com>
2021-09-12 03:53:03 +0530

    Update App.js

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
-import { robots } from '../robots';
+// import { robots } from '../robots';
 import './App.css';
 
 class App extends Component {
